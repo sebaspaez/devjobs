4055728be10b17ac7af34bc86b28934dfd2b5063
Sebastian Paez <sebaspaez25@gmail.com>
2019-06-13 16:54:01 -0400

    modelo de vacantes, formulario para agregar vacantes

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config({path: 'variables.env'});
+
+mongoose.connect(process.env.DATABASE, {useNewUrlParser:true});
+
+mongoose.connection.on('error', (error) => {
+    console.log(error);
+})
+
+//importar los modelos
+require('../models/Vacantes');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeControllers');
+const vacantesController = require('../controllers/vacantesController');
 
 module.exports = () => {
     router.get('/', homeController.mostrarTrabajos);
+
+    //crear vacantes
+    router.get('/vacantes/nueva', vacantesController.formularioNuevaVacante)
 
     return router;
 }
